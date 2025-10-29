be28fd386b17a18cf915d643672d564ea18245f9
Triovians <connect@triovative.com>
2025-10-29 22:16:08 +0530

    Refactor EKS IAM roles and update node group settings
    Refactor IAM roles and policies for EKS cluster and node group. Update scaling configuration for node group.

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,12 +3,6 @@ variable "version" { default = "1.27" }
 variable "private_subnets" { type = list(string) }
 variable "public_subnets" { type = list(string) }
 
-# IAM role for EKS
-resource "aws_iam_role" "eks_cluster" {
-  name = "${var.cluster_name}-eks-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
-}
-
 data "aws_iam_policy_document" "eks_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -16,23 +10,28 @@ data "aws_iam_policy_document" "eks_assume_role" {
   }
 }
 
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.cluster_name}-eks-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   version  = var.version
-
   role_arn = aws_iam_role.eks_cluster.arn
-
   vpc_config {
     subnet_ids = var.private_subnets
   }
-
-  depends_on = []
-}
-
-# Node Group IAM role
-resource "aws_iam_role" "node_group" {
-  name = "${var.cluster_name}-node-group-role"
-  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
 }
 
 data "aws_iam_policy_document" "node_assume_role" {
@@ -42,39 +41,35 @@ data "aws_iam_policy_document" "node_assume_role" {
   }
 }
 
-# Attach managed policies to roles (minimal)
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  role       = aws_iam_role.node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+resource "aws_iam_role" "node_group" {
+  name = "${var.cluster_name}-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
 }
 
-# Managed node group
+resource "aws_iam_role_policy_attachment" "node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ])
+  role       = aws_iam_role.node_group.name
+  policy_arn = each.value
+}
+
 resource "aws_eks_node_group" "workers" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-ng"
+  node_group_name = \"${var.cluster_name}-ng\"
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = var.private_subnets
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = [\"t3.medium\"]
 }
 
-output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
-output "cluster_name" { value = aws_eks_cluster.this.name }
+output \"cluster_name\" { value = aws_eks_cluster.this.name }
+output \"cluster_endpoint\" { value = aws_eks_cluster.this.endpoint }
+output \"node_group_name\" { value = aws_eks_node_group.workers.node_group_name }

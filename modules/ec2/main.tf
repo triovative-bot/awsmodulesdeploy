2b63e2830849134190609a9b5c228a9dc5c7b200
Triovians <connect@triovative.com>
2025-10-29 22:13:19 +0530

    Modify EC2 instance tags and outputs
    Updated EC2 instance tags and added private IP output.

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,14 +5,16 @@ variable "key_name" {}
 variable "vpc_security_group_ids" { type = list(string) }
 
 resource "aws_instance" "this" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  key_name               = var.key_name
-  vpc_security_group_ids = var.vpc_security_group_ids
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  vpc_security_group_ids      = var.vpc_security_group_ids
   associate_public_ip_address = true
-  tags = { Name = "ec2-instance" }
+
+  tags = { Name = "public-ec2" }
 }
 
 output "instance_id" { value = aws_instance.this.id }
 output "public_ip" { value = aws_instance.this.public_ip }
+output "private_ip" { value = aws_instance.this.private_ip }

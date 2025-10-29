a1dbba12d1fb781f29020bacaf26279fb9d394c8
Triovians <connect@triovative.com>
2025-10-29 22:17:17 +0530

    Change default name variable from 'demo' to 'prod'

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,10 +1,10 @@
 variable "region" { default = "us-east-1" }
-variable "name" { default = "demo" }
+variable "name" { default = "prod" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "azs" { default = ["us-east-1a","us-east-1b"] }
 variable "public_subnets_cidrs" { default = ["10.0.1.0/24","10.0.2.0/24"] }
 variable "private_subnets_cidrs" { default = ["10.0.101.0/24","10.0.102.0/24"] }
 variable "admin_cidr" { default = "0.0.0.0/0" }
-variable "ec2_ami" { default = "ami-0c94855ba95c71c99" } # change per region
+variable "ec2_ami" { default = "ami-0c94855ba95c71c99" }
 variable "ec2_instance_type" { default = "t3.micro" }
 variable "ssh_key_name" { default = "my-key" }

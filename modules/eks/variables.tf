3c79f00c8817a10a2d6fe33dbc330fe9d79f93d1
Triovians <connect@triovative.com>
2025-10-30 00:00:31 +0530

    Update default EKS cluster version to 1.33

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.27"
+  default     = "1.33"
 }
 
 variable "private_subnets" {

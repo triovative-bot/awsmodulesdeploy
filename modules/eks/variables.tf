8345b5708b1251afa8398d37ca2a3631d2c6ed8c
Triovians <connect@triovative.com>
2025-10-29 22:29:51 +0530

    Add variables for EKS module configuration

---
+++ modules/eks/variables.tf
@@ -0,0 +1,20 @@
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.27"
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}

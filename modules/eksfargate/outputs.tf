47cb232dce14f7083baf14a920786a37e42b133d
Triovians <connect@triovative.com>
2025-11-01 13:29:30 +0530

    Update outputs.tf

--- modules/eksfargate/outputs.tf
+++ modules/eksfargate/outputs.tf
@@ -1,19 +1,39 @@
-############################################
-# modules/eksfargate/outputs.tf
-############################################
-
-output "cluster_name" {
-  value = aws_eks_cluster.this.name
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
 }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.28"
 }
 
-output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
+variable "private_subnets" {
+  description = "List of private subnet IDs for the EKS cluster"
+  type        = list(string)
 }
 
-output "fargate_profile_names" {
-  value = [for p in aws_eks_fargate_profile.default : p.fargate_profile_name]
+variable "public_subnets" {
+  description = "List of public subnet IDs (not used directly in EKS but available for reference)"
+  type        = list(string)
+  default     = []
+}
+
+variable "fargate_namespaces" {
+  description = "List of namespaces to run on Fargate"
+  type        = list(string)
+  default     = ["default", "kube-system"]
+}
+
+variable "public_access_cidrs" {
+  description = "List of CIDR blocks that can access the EKS public API server endpoint"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  description = "Additional tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }

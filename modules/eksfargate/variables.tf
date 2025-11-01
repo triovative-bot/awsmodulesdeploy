f9a434109fc2d66bd9b842487a44a42943629704
Triovians <connect@triovative.com>
2025-11-01 13:29:45 +0530

    Update variables.tf

--- modules/eksfargate/variables.tf
+++ modules/eksfargate/variables.tf
@@ -1,45 +1,39 @@
-############################################
-# modules/eksfargate/variables.tf
-############################################
-
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
 
-variable "cluster_role_arn" {
-  description = "IAM role ARN for the EKS control plane"
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
   type        = string
+  default     = "1.28"
 }
 
-variable "pod_execution_role_arn" {
-  description = "IAM role ARN for Fargate pods"
-  type        = string
-}
-
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs for EKS"
+variable "private_subnets" {
+  description = "List of private subnet IDs for the EKS cluster"
   type        = list(string)
 }
 
-variable "fargate_profiles" {
-  description = "Map of Fargate profiles with subnet_ids and selectors"
-  type = map(object({
-    subnet_ids = list(string)
-    selectors  = list(object({
-      namespace = string
-      labels    = optional(map(string))
-    }))
-  }))
+variable "public_subnets" {
+  description = "List of public subnet IDs (not used directly in EKS but available for reference)"
+  type        = list(string)
+  default     = []
+}
+
+variable "fargate_namespaces" {
+  description = "List of namespaces to run on Fargate"
+  type        = list(string)
+  default     = ["default", "kube-system"]
+}
+
+variable "public_access_cidrs" {
+  description = "List of CIDR blocks that can access the EKS public API server endpoint"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
 
 variable "tags" {
-  description = "Common tags"
+  description = "Additional tags to apply to all resources"
   type        = map(string)
   default     = {}
-}
-variable "env" {
-  description = "Environment name (e.g., dev, test, prod)"
-  type        = string
-  default     = "dev"
 }

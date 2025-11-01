163cd9c687d47a0c79f3bdfd56771771f69cac77
Triovians <connect@triovative.com>
2025-11-01 13:29:17 +0530

    Update main.tf

--- modules/eksfargate/main.tf
+++ modules/eksfargate/main.tf
@@ -1,90 +1,105 @@
-############################################
-# modules/eksfargate/main.tf
-############################################
-
+# EKS Cluster
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  role_arn = var.cluster_role_arn
+  version  = var.cluster_version
+  role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids              = var.private_subnets
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = var.public_access_cidrs
   }
 
-  tags = merge(var.tags, {
-    "Name" = var.cluster_name
-  })
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSVPCResourceController,
+  ]
+
+  tags = var.tags
 }
 
-# Create Fargate profiles
-resource "aws_eks_fargate_profile" "default" {
-  for_each = var.fargate_profiles
+# EKS Fargate Profile
+resource "aws_eks_fargate_profile" "this" {
+  for_each = toset(var.fargate_namespaces)
 
   cluster_name           = aws_eks_cluster.this.name
-  fargate_profile_name   = each.key
-  pod_execution_role_arn = var.pod_execution_role_arn
-  subnet_ids             = each.value.subnet_ids
-  selectors              = each.value.selectors
+  fargate_profile_name   = "${var.cluster_name}-${replace(each.value, "_", "-")}"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution.arn
+  subnet_ids             = var.private_subnets
 
-  tags = merge(var.tags, {
-    "FargateProfile" = each.key
-  })
+  selector {
+    namespace = each.value
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.fargate_pod_execution,
+  ]
+
+  tags = var.tags
 }
 
-############################################
-# IAM Role for EKS Control Plane
-############################################
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.name}-eks-cluster-role"
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
     Statement = [{
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "eks.amazonaws.com"
       }
-      Action = "sts:AssumeRole"
     }]
+    Version = "2012-10-17"
   })
 
-  tags = {
-    Name = "${var.name}-eks-cluster-role"
-  }
+  tags = var.tags
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
-############################################
 # IAM Role for Fargate Pod Execution
-############################################
-resource "aws_iam_role" "fargate_pod_role" {
-  name = "${var.name}-fargate-pod-role"
+resource "aws_iam_role" "fargate_pod_execution" {
+  name = "${var.cluster_name}-fargate-pod-execution-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
     Statement = [{
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "eks-fargate-pods.amazonaws.com"
       }
-      Action = "sts:AssumeRole"
     }]
+    Version = "2012-10-17"
   })
 
-  tags = {
-    Name = "${var.name}-fargate-pod-role"
-  }
+  tags = var.tags
 }
 
+# IAM Policy Attachments for Cluster
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.cluster.name
+}
+
+# IAM Policy Attachment for Fargate
 resource "aws_iam_role_policy_attachment" "fargate_pod_execution" {
-  role       = aws_iam_role.fargate_pod_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution.name
+}
+
+# Kubernetes Provider Configuration
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+# Kubernetes Provider (for future use with kubectl manifests)
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }

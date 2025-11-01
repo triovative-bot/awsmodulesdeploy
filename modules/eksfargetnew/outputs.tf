5ebd968fdcfa9ffb2175573bb9ff9f14324fb081
Triovians <connect@triovative.com>
2025-11-01 11:53:36 +0530

    Update outputs.tf

--- modules/eksfargetnew/outputs.tf
+++ modules/eksfargetnew/outputs.tf
@@ -1,15 +1,28 @@
+######################################
+# outputs.tf
+######################################
+
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  description = "EKS API endpoint"
+  value       = aws_eks_cluster.this.endpoint
 }
 
-output "cluster_arn" {
-  value = aws_eks_cluster.this.arn
+output "cluster_role_arn" {
+  description = "EKS IAM role ARN"
+  value       = aws_iam_role.eks_cluster.arn
 }
 
-output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.this.fargate_profile_name
+output "fargate_profile_private_name" {
+  description = "Name of the private Fargate profile"
+  value       = aws_eks_fargate_profile.private.fargate_profile_name
+}
+
+output "fargate_profile_public_name" {
+  description = "Name of the public Fargate profile (if created)"
+  value       = try(aws_eks_fargate_profile.public[0].fargate_profile_name, null)
 }

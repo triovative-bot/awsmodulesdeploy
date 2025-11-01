8e00ec9e35300847d56f35d73db9878e4c90a324
Triovians <connect@triovative.com>
2025-11-01 12:22:01 +0530

    Update main.tf

--- modules/eksfargate/main.tf
+++ modules/eksfargate/main.tf
@@ -29,3 +29,62 @@ resource "aws_eks_fargate_profile" "default" {
     "FargateProfile" = each.key
   })
 }
+
+############################################
+# IAM Role for EKS Control Plane
+############################################
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "${var.name}-eks-cluster-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+############################################
+# IAM Role for Fargate Pod Execution
+############################################
+resource "aws_iam_role" "fargate_pod_role" {
+  name = "${var.name}-fargate-pod-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "${var.name}-fargate-pod-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution" {
+  role       = aws_iam_role.fargate_pod_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}

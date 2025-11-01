0b7b9619e6f732268c297347ad4a6f20476903aa
Triovians <connect@triovative.com>
2025-11-01 11:52:56 +0530

    Update main.tf

--- modules/eksfargetnew/main.tf
+++ modules/eksfargetnew/main.tf
@@ -1,42 +1,114 @@
-resource "aws_iam_role" "eks_fargate" {
-  name = "${var.name}-fargate-role"
+######################################
+# main.tf
+######################################
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "fargate_policy" {
-  role       = aws_iam_role.eks_fargate.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+# --------------------------------------------
+# IAM Role for EKS Cluster
+# --------------------------------------------
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
 }
 
+resource "aws_iam_role" "eks_cluster" {
+  name               = "${var.cluster_name}-eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# --------------------------------------------
+# EKS Cluster
+# --------------------------------------------
 resource "aws_eks_cluster" "this" {
-  name     = "${var.name}-fargate"
-  role_arn = aws_iam_role.eks_fargate.arn
-  version  = var.kubernetes_version
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.private_subnets
   }
 
-  depends_on = [aws_iam_role_policy_attachment.fargate_policy]
+  version = var.cluster_version
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "aws_eks_fargate_profile" "this" {
-  cluster_name           = aws_eks_cluster.this.name
-  fargate_profile_name   = "${var.name}-profile"
-  pod_execution_role_arn = aws_iam_role.eks_fargate.arn
-  subnet_ids             = var.subnet_ids
+# --------------------------------------------
+# IAM Role for Fargate Profile
+# --------------------------------------------
+data "aws_iam_policy_document" "fargate_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks-fargate-pods.amazonaws.com"]
+    }
+  }
+}
 
-  selector {
-    namespace = "default"
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name               = "${var.cluster_name}-fargate-pod-role"
+  assume_role_policy = data.aws_iam_policy_document.fargate_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
+
+# --------------------------------------------
+# EKS Fargate Profiles (private + optional public)
+# --------------------------------------------
+# Default Fargate profile for private workloads
+resource "aws_eks_fargate_profile" "private" {
+  cluster_name           = aws_eks_cluster.this.name
+  fargate_profile_name   = "${var.cluster_name}-fargate-private"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = var.private_subnets
+
+  dynamic "selector" {
+    for_each = var.fargate_namespaces
+    content {
+      namespace = selector.value
+    }
   }
 
-  depends_on = [aws_eks_cluster.this]
+  depends_on = [
+    aws_iam_role_policy_attachment.fargate_pod_execution_role_policy
+  ]
+}
+
+# Optional public namespace profile
+resource "aws_eks_fargate_profile" "public" {
+  count                  = length(var.public_subnets) > 0 ? 1 : 0
+  cluster_name           = aws_eks_cluster.this.name
+  fargate_profile_name   = "${var.cluster_name}-fargate-public"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = var.public_subnets
+
+  selector {
+    namespace = "public"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.fargate_pod_execution_role_policy
+  ]
 }

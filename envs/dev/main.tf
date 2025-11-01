fc6622b96c58e14ec89ff508e605899c225f6dd1
Triovians <connect@triovative.com>
2025-11-01 18:23:54 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -7,7 +7,40 @@ module "vpc" {
   private_subnets_cidrs = var.private_subnets_cidrs
 }
 
-
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = module.vpc.vpc_id
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.admin_cidr]
+  }
+ 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+module "ec2" {
+  source                 = "../../modules/ec2"
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  subnet_id              = element(module.vpc.public_subnets, 0)
+  key_name               = var.ssh_key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
 ###======================================================================
 
 # Security Group for EKS Cluster

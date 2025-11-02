89f191f6efbd23ad7248257fc64596822713b82c
Triovians <connect@triovative.com>
2025-11-02 18:18:10 +0530

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,5 +1,17 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
+
+
+
+#terraform {
+#  backend "local" {
+#    path = "terraform.tfstate"
+#  }
+#}
